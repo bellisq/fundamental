37503d1256fc56daa726ed523085c61758b430db
Showsay You <akizuki.c10.l65@gmail.com>
2017-12-26 20:11:52 +0900

    Update ConfigAbstract

--- src/Config/ConfigAbstract.php
+++ src/Config/ConfigAbstract.php
@@ -2,6 +2,7 @@
 
 namespace Bellisq\Fundamental\Config;
 
+use Bellisq\Fundamental\Exceptions\InvalidConfigException;
 use Bellisq\Fundamental\Exceptions\MissingConfigException;
 use Strict\Property\Utility\ReadonlyPropertyContainer;
 
@@ -20,6 +21,11 @@ abstract class ConfigAbstract
     /** @var array */
     private $env;
 
+    /**
+     * ConfigAbstract constructor.
+     *
+     * @param array $env
+     */
     final public function __construct(array $env)
     {
         $this->env = $env;
@@ -28,10 +34,53 @@ abstract class ConfigAbstract
 
     abstract protected function initialize(array $env);
 
-    protected function existOrFail(string $key): void
+    /**
+     * Check if a specific env variable is set or not.
+     * If it isn't set, throw an exception.
+     *
+     * @param string $key
+     *
+     * @throws MissingConfigException
+     */
+    final protected function existOrFail(string $key): void
     {
         if (!isset($this->env[$key])) {
             throw new MissingConfigException($key, static::class);
         }
+    }
+
+    /**
+     * Get bool value from env variables.
+     *
+     * @param string $key
+     * @param bool $default
+     * @return bool
+     */
+    final protected function getBool(string $key, bool $default = false): bool
+    {
+        if (!isset($this->env[$key]) or !is_string($this->env[$key])) {
+            return $default;
+        }
+
+        $t = strtolower(trim($this->env[$key]));
+
+        $allowed = [
+            'true' => true,
+            'on' => true,
+            '1' => true,
+            'false' => false,
+            'off' => false,
+            '0' => false,
+        ];
+        if (isset($allowed[$t])) {
+            return $allowed[$t];
+        }
+
+        if (is_numeric($t)) {
+            // if $t == 0, false is already returned.
+            return true;
+        }
+
+        throw new InvalidConfigException($key, static::class);
     }
 }
