f96b23d09d6342e21ab2cbd2456e32fb11170290
Showsay You <akizuki.c10.l65@gmail.com>
2018-03-23 11:27:53 +0900

    Define ModelAbstract

---
+++ src/ModelAbstract.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bellisq\Fundamental;
+
+
+/**
+ * [Abstract Class] Model
+ *
+ * @author Showsay You <akizuki.c10.l65@gmail.com>
+ * @copyright 2018 Bellisq. All Rights Reserved.
+ * @package bellisq/fundamental
+ * @since 2.1.0
+ */
+abstract class ModelAbstract
+{
+
+}
